989c374c6e46783dd2de2021c21cd339c72fd4af
TIm Raynor <55665132+timraynor80@users.noreply.github.com>
2021-05-06 18:26:52 -0500

    202105061826

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,6 +1,13 @@
 import os
 import csv
 divider = ('-' * 50)
+election_csv = os.path.join('..', '..', 'Instructions', 'PyPoll', 'Resources', 'election_data.csv')
+with open(election_csv, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
+    # for row in csvreader:
+    #     print(row)
+    print(csv_header)
 
-print(f'Election Results')
-print(divider)
+# print(f'Election Results')
+# print(divider)
