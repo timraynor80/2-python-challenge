13c16962436b42f1977cfc2fa20518a3d949f6c0
TIm Raynor <55665132+timraynor80@users.noreply.github.com>
2021-05-11 08:10:51 -0500

    20210511

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,13 +1,28 @@
 import os
 import csv
 divider = ('-' * 50)
+voter_id = []
+votes = []
+candidates = []
+output = open('output.txt', 'w')
 election_csv = os.path.join('..', '..', 'Instructions', 'PyPoll', 'Resources', 'election_data.csv')
 with open(election_csv, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
-    # for row in csvreader:
-    #     print(row)
-    print(csv_header)
-
-# print(f'Election Results')
-# print(divider)
+    # '${:,}'.format(total_votes)
+    for row in csvreader:
+        voter_id.append(row[0])
+        votes.append(row[2])
+        if row[2] not in candidates:
+            candidates.append(row[2])
+total_votes = '{:,}'.format(len(votes))
+print(f'Election Results')
+print(divider)
+print(f'Total Votes: {total_votes}')
+print(divider)
+for candidate in candidates:
+    tally = '{:,}'.format(votes.count(candidate))
+    share = '{0:.2f}%'.format((votes.count(candidate) / len(votes)) * 100)
+    print(f'{candidate}: {share} ({tally})')
+print(divider)
+print(f'Winner: {max(set(votes), key= votes.count)}')
